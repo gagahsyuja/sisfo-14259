09e8b29bd31f0cc4a742476c0730cbcbff22ead0
Discipl4 <gghssa1@gmail.com>
2022-12-27 19:44:02 +0700

    Better popup behavior

--- php/help.php
+++ php/help.php
@@ -7,7 +7,7 @@ if (isset($_POST['two']))
 
     echo '<h2>Hi <span style="color: #fabd2f;">' . $name . '</span>,</h2>';
     echo '<h2>With <span style="color: #fabd2f;">' . $vram . '</span> VRAM of yours,</h2>';
-    echo '<h2>We recommend you <a href="./index.html#equihash">Equihash</a></h2>';
+    echo '<h2>We recommend you <a href="./index.html#equihash" target="_blank">Equihash</a></h2>';
 
     echo '<button onclick="closePopup()" class="close">Close</button>';
 	echo '<style>.recommend{display: block;} .recommend-outside{display: block;}</style>';
@@ -20,7 +20,7 @@ if (isset($_POST['four']))
 
     echo '<h2>Hi <span style="color: #fabd2f;">' . $name . '</span>,</h2>';
     echo '<h2>With <span style="color: #fabd2f;">' . $vram . '</span> VRAM of yours,</h2>';
-    echo '<h2>We recommend you <a href="./index.html#equihash">Equihash</a> or <a href="./index.html#kawpow">KawPow</a></h2>';
+    echo '<h2>We recommend you <a href="./index.html#equihash" target="_blank">Equihash</a> or <a href="./index.html#kawpow" target="_blank">KawPow</a></h2>';
 
     echo '<button onclick="closePopup()" class="close">Close</button>';
 	echo '<style>.recommend{display: block;} .recommend-outside{display: block;}</style>';
@@ -33,7 +33,7 @@ if (isset($_POST['six']))
 
     echo '<h2>Hi <span style="color: #fabd2f;">' . $name . '</span>,</h2>';
     echo '<h2>With <span style="color: #fabd2f;">' . $vram . '</span> VRAM of yours,</h2>';
-    echo '<h2>We recommend you <a href="./index.html#kawpow">KawPow</a> or <a href="./index.html#ethash">Ethash</a></h2>';
+    echo '<h2>We recommend you <a href="./index.html#kawpow" target="_blank">KawPow</a> or <a href="./index.html#ethash" target="_blank">Ethash</a></h2>';
 
     echo '<button onclick="closePopup()" class="close">Close</button>';
 	echo '<style>.recommend{display: block;} .recommend-outside{display: block;}</style>';
@@ -46,7 +46,7 @@ if (isset($_POST['eight']))
 
     echo '<h2>Hi <span style="color: #fabd2f;">' . $name . '</span>,</h2>';
     echo '<h2>With <span style="color: #fabd2f;">' . $vram . '</span> VRAM of yours,</h2>';
-    echo '<h2>We recommend you <a href="./index.html#ethash">Ethash</a></h2>';
+    echo '<h2>We recommend you <a href="./index.html#ethash" target="_blank">Ethash</a></h2>';
 
     echo '<button onclick="closePopup()" class="close">Close</button>';
 	echo '<style>.recommend{display: block;} .recommend-outside{display: block;}</style>';
