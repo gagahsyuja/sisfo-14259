2d554d8a7a43e4a83534de85c5d2bb18b5232a9a
gagahsyuja <gghssa1@gmail.com>
2023-04-25 23:32:32 +0700

    improved index/home logic

--- php/index.php
+++ php/index.php
@@ -76,87 +76,134 @@ foreach ($getAlgo as $algo)
     
     $sql = "SELECT * FROM `coin` WHERE `coin_algo` = '$algo'";
     $result = mysqli_query($conn, $sql);
-
-    while ($row = mysqli_fetch_assoc($result))
+    $amount = mysqli_num_rows($result);
+    
+    $loop = 0;
+    $even = 3;
+    for (; $even < $amount; $even += 3)
     {
-        $getCoin[] = $row;
-    }
-
-    foreach ($getCoin as $coin)
-    {
-        $name = $coin['coin_name'];
-
-        echo
-        '
-                <td><h3>' . $name . '</h3></td>
-        ';
-    }
-
-    echo
-    '
-            </tr>
-            <tr class="table-logo">
-    ';
-
-    foreach ($getCoin as $coin)
-    {
-        $name = $coin['coin_name'];
-        $logo = $coin['coin_image'];
-        $short = $coin['coin_short_name'];
-        
-        echo
-        '
-                <td><a href="./coins/' . $short . '.php"><img src="' . $logo . '" alt="image of ' . $name . '" width="18%"></a></td>
-        ';
-    }
-
-    echo
-    '
-            </tr>
-            <tr class="wish-box">
-                <form action="./php/' . $algo . '.php" method="post">
-    ';
-
-    foreach ($getCoin as $coin)
-    {
-        $short = $coin['coin_short_name'];
-
-        $sql = "SELECT `watchlist`.*, `coin`.`coin_id`, `coin`.`coin_short_name`
-                FROM `watchlist`
-                INNER JOIN `coin`
-                ON `watchlist`.`watchlist_id` = `coin`.`coin_id`
-                WHERE `uname` = '$dbname'
-                AND `coin_short_name` = '$short'
-                ";
-
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0)
+        if ($even < $amount)
         {
-            echo
-            '
-                        <td><button type="submit" name="submit' . ucfirst($short) . '"><i class="fa-sharp fa-solid fa-eye fa-2x"></i></button></td>
-            ';
+            $loop += 1;
         }
 
         else
         {
-            echo
-            '
-                        <td><button type="submit" name="submit' . ucfirst($short) . '"><i class="fa-regular fa-eye fa-2x"></i></button></td>
-            ';
+            break;
         }
     }
 
-    echo
-    '
-                </form>
-            </tr>
-        </table>
-    ';
+    $notEven = $even - $amount;
+
+    while ($row = mysqli_fetch_assoc($result))
+    {
+        $coin[] = $row;
+    }
+
+    for ($i = 0; $i <= $loop; $i++)
+    {
+        $count = 3 * ($i + 1);
+        $init = ($count - 3);
+
+        for ($j = $init; $j < $count AND $j < $amount; $j++)
+        {
+            $name = $coin[$j]['coin_name'];
+
+            echo
+            '
+                    <td><h3>' . $name . '</h3></td>
+            ';
+        }
         
+        // Filling up the rest of the table
+        // if the coin amount modulus by 3 is not equal to 0
+
+        for ($x = 0; $x < $notEven; $x++)
+        {
+            if ($i == $loop)
+            {
+                echo
+                '
+                    <td></td>
+                ';
+            }
+        }
+
+        echo
+        '
+                </tr>
+                <tr class="table-logo">
+        ';
+        
+        for ($k = $init; $k < $count AND $k < $amount; $k++)
+        {
+            $name = $coin[$k]['coin_name'];
+            $logo = $coin[$k]['coin_image'];
+            $short = $coin[$k]['coin_short_name'];
+            
+            echo
+            '
+                    <td><a href="./coins/' . $short . '.php"><img src="' . $logo . '" alt="image of ' . $name . '" width="18%"></a></td>
+            ';
+        }
+    
+        echo
+        '
+                </tr>
+                <tr class="wish-box">
+                    <form action="./php/' . $algo . '.php" method="post">
+        ';
+    
+        for ($l = $init; $l < $count AND $l < $amount; $l++)
+        {
+            $short = $coin[$l]['coin_short_name'];
+    
+            $sql = "SELECT `watchlist`.*, `coin`.`coin_id`, `coin`.`coin_short_name`
+                    FROM `watchlist`
+                    INNER JOIN `coin`
+                    ON `watchlist`.`watchlist_id` = `coin`.`coin_id`
+                    WHERE `uname` = '$dbname'
+                    AND `coin_short_name` = '$short'
+                    ";
+    
+            $result = mysqli_query($conn, $sql);
+    
+            if (mysqli_num_rows($result) > 0)
+            {
+                echo
+                '
+                        <td><button type="submit" name="submit' . ucfirst($short) . '"><i class="fa-sharp fa-solid fa-eye fa-2x"></i></button></td>
+                ';
+            }
+    
+            else
+            {
+                echo
+                '
+                        <td><button type="submit" name="submit' . ucfirst($short) . '"><i class="fa-regular fa-eye fa-2x"></i></button></td>
+                ';
+            }
+        }
+        
+        echo
+        '
+                    </form>
+                </tr>
+            </table>
+        ';
+
+        if ($loop != 0 AND $i != $loop)
+        echo
+        '   
+            <br>
+            <table class="table-home">
+                <tr class="table-coin-name">
+        ';
+    }
+
     $contents = FALSE;
-    unset($getCoin);
+    
+    unset($coin);
 }
 
 echo
